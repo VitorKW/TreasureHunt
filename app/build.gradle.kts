a0a09cb565709244b571ac43e367655702ee41fd
vitor wessler <vitorwessler@gmail.com>
2025-03-28 15:02:00 -0300

    Adição das funções Extras do Exercício

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,20 +43,13 @@ android {
 dependencies {
     val nav_version = "2.7.7"
 
-    // Jetpack Compose integration
-    implementation("androidx.navigation:navigation-compose:$nav_version")
 
-    // Views/Fragments integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.navigation:navigation-fragment:$nav_version")
     implementation("androidx.navigation:navigation-ui:$nav_version")
-
-    // Feature module support for Fragments
     implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
-
-    // Testing Navigation
     androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
-
-    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation ("com.google.accompanist:accompanist-navigation-animation:0.32.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/treasurehunt/ui/theme/Tesouro.kt
+++ app/src/main/java/com/example/treasurehunt/ui/theme/Tesouro.kt
@@ -5,17 +5,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.treasurehunt.R
+
 
 @Composable
 fun TelaInicial(onNavigateToPista1: () -> Unit) {
@@ -24,11 +34,14 @@ fun TelaInicial(onNavigateToPista1: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Text("Caça ao Tesouro",
+            fontSize = 40.sp)
         Button(
             onClick = { onNavigateToPista1() },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
         ) {
-            Text(text = "Começar Caça ao Tesouro", fontSize = 36.sp)
+            Text(text = "Começar!", fontSize = 34.sp,
+                textAlign = TextAlign.Center)
         }
 
 
@@ -61,8 +74,9 @@ fun TelaPista1(onNavigateToPista2: () -> Unit, onNavigateToTelaInicial: () -> Un
         Row {
             Text(
                 text = "Pista 1: Sou essencial para a vida, mas posso afogar você.",
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Serif
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Serif,
+                textAlign = TextAlign.Center
             )
         }
     }
@@ -94,8 +108,9 @@ fun TelaPista2(onNavigateToPista3: () -> Unit, onNavigateToPista1: () -> Unit) {
         Row {
             Text(
                 text = "Pista 2: Posso estar no estado sólido, líquido ou gasoso.",
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Serif
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Serif,
+                textAlign = TextAlign.Center
             )
         }
     }
@@ -124,51 +139,50 @@ fun TelaPista3(onNavigateToResposta: () -> Unit, onNavigateToPista2: () -> Unit)
         Row {
             Text(
                 text = "Pista 3: Sem mim, os oceanos não existiriam.",
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Serif
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Serif,
+                textAlign = TextAlign.Center
             )
         }
     }
 }
 
 @Composable
-fun TelaResposta(onNavigateToCerta: () -> Unit, onNavigateToErrada: () -> Unit) {
+fun TelaResposta(onNavigateToTelaCerta: () -> Unit, onNavigateToTelaErrada: () -> Unit) {
+    var resposta by remember { mutableStateOf("") }
+    val respostaCorreta = "Agua"
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Resposta para a Charada", fontSize = 36.sp, fontFamily = FontFamily.Serif)
+        Text(text = "Resposta para a Charada",
+            fontSize = 36.sp,
+            fontFamily = FontFamily.Serif,
+            textAlign = TextAlign.Center)
+
+        OutlinedTextField(
+            value = resposta,
+            onValueChange = { resposta = it },
+            label = { Text("Digite a resposta") },
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
         Button(
-            onClick = { onNavigateToErrada() },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+            onClick = {
+                if (resposta.equals(respostaCorreta, ignoreCase = true)) {
+                    onNavigateToTelaCerta()
+                } else {
+                    onNavigateToTelaErrada()
+                }
+            },
+            modifier = Modifier.padding(top = 16.dp)
         ) {
-            Text(text = "Terra")
-        }
-        Button(
-            onClick = { onNavigateToCerta() },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
-        ) {
-            Text(text = "Água")
-        }
-        Button(
-            onClick = { onNavigateToErrada() },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
-        ) {
-            Text(text = "Fogo")
-        }
-        Button(
-            onClick = { onNavigateToErrada() },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
-        ) {
-            Text(text = "Ar")
-        }
-        Row {
-            Text(
-                text = "Escolha com Cuidado!",
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Serif
-            )
+            Text("Confirmar")
         }
     }
 }
@@ -190,7 +204,7 @@ fun TelaCerta(onNavigateToTelaInicial: () -> Unit) {
 
         Text(
             text = "Meus Parabéns, Você Conseguiu!",
-            fontSize = 30.sp,
+            fontSize = 24.sp,
             fontFamily = FontFamily.Serif
         )
 
@@ -220,7 +234,7 @@ fun TelaErrada(onNavigateToTelaInicial: () -> Unit) {
 
         Text(
             text = "Você Errou, lá vai bomba!",
-            fontSize = 30.sp,
+            fontSize = 24.sp,
             fontFamily = FontFamily.Serif
         )
 
